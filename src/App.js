d81282d54b7b3984d584a7bd568becc65b90bc3b
evgar <eugene.garichev@gmail.com>
2018-07-31 13:50:24 +0300

    update dependencies

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import Table from './Table';
 import ExportToJSON from './ExportToJSON'
 import ExportToCSV from './ExportToCSV'
-
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 class App extends Component {
   render() {
